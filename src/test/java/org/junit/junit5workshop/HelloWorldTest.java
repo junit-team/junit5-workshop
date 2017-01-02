6c4d92c54e563b7d4cf082b47e7bfc91d529e82e
nipa <nipa@codefx.org>
2017-01-02 18:53:00 +0100

    Add HelloWorldTest

---
+++ src/test/java/org/junit/junit5workshop/HelloWorldTest.java
@@ -0,0 +1,32 @@
+package org.junit.junit5workshop;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Typical "Hello World"; also shows that Mockito and AssertJ are compatible.
+ */
+class HelloWorldTest {
+
+	@Test
+	void helloJUnit5() {
+		System.out.println("Hello, JUnit 5.");
+	}
+
+	@Test
+	void usingOtherLibs() {
+		List mockedList = when(
+				mock(List.class)
+						.isEmpty())
+				.thenReturn(true)
+				.getMock();
+		// passes because we just mocked 'mockedList.isEmpty' to return true
+		assertThat(mockedList).isEmpty();
+	}
+
+}
