fd8391fd10a79ef158ff635d20415b1b0bb042e1
Marc Philipp <mail@marcphilipp.de>
2017-01-25 14:21:21 +0100

    Add example for junit-platform-runner

---
+++ src/test/java/org/junit/junit5workshop/_5_compatibility/AllTests.java
@@ -0,0 +1,10 @@
+package org.junit.junit5workshop._5_compatibility;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.runner.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("org.junit.junit5workshop")
+public class AllTests {
+}
---
+++ src/test/java/org/junit/junit5workshop/_5_compatibility/OldTest.java
@@ -0,0 +1,13 @@
+package org.junit.junit5workshop._5_compatibility;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OldTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
